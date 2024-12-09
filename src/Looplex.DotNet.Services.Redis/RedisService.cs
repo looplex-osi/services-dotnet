3d3d037efcdc1d9c2371ed6ef9832e529dd98da4
Guilherme Câmara <guilherme.camara@outlook.com.br>
2024-12-09 09:28:49 -0300

    fix(redisservice): laz load database for redis service

--- src/Looplex.DotNet.Services.Redis/RedisService.cs
+++ src/Looplex.DotNet.Services.Redis/RedisService.cs
@@ -7,24 +7,36 @@ public class RedisService(IConnectionMultiplexer connectionMultiplexer) : IRedis
 {
     const string? KeyCannotBeNullOrEmpty = "Key cannot be null or empty.";
 
-    private readonly IDatabase _database = connectionMultiplexer.GetDatabase(); // Get a database instance
+    private IDatabase? _database;
+
+    private IDatabase Database
+    {
+        get
+        {
+            if (_database == null)
+            {
+                _database = connectionMultiplexer.GetDatabase(); // Get a database instance
+            }
+            return _database;
+        }
+    } 
 
     public async Task SetAsync(string key, string value)
     {
         if (string.IsNullOrEmpty(key)) throw new ArgumentException(KeyCannotBeNullOrEmpty);
-        await _database.StringSetAsync(key, value);
+        await Database.StringSetAsync(key, value);
     }
 
     public async Task<string?> GetAsync(string key)
     {
         if (string.IsNullOrEmpty(key)) throw new ArgumentException(KeyCannotBeNullOrEmpty);
-        var value = await _database.StringGetAsync(key);
+        var value = await Database.StringGetAsync(key);
         return value.IsNullOrEmpty ? null : value.ToString();
     }
 
     public async Task<bool> DeleteAsync(string key)
     {
         if (string.IsNullOrEmpty(key)) throw new ArgumentException(KeyCannotBeNullOrEmpty);
-        return await _database.KeyDeleteAsync(key);
+        return await Database.KeyDeleteAsync(key);
     }
 }
