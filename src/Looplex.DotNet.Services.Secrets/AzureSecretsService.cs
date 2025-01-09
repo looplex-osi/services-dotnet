f2971e02ebceb7c6d541bcc3081758c34dd1fd98
Fernando Cruz <fernando.cruz@looplex.com.br>
2025-01-09 19:33:19 -0300

    Implementação do teste unitário do serviço AzureSecretsService

--- src/Looplex.DotNet.Services.Secrets/AzureSecretsService.cs
+++ src/Looplex.DotNet.Services.Secrets/AzureSecretsService.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Security.KeyVault.Secrets;
+﻿using Azure;
+using Azure.Security.KeyVault.Secrets;
 using Looplex.DotNet.Core.Application.Abstractions.Services;
 
 namespace Looplex.DotNet.Services.Secrets;
@@ -8,11 +9,17 @@ public class AzureSecretsService (SecretClient secretClient) : ISecretsService
     public async Task<string?> GetSecretAsync(string secretName)
     {          
         if (string.IsNullOrEmpty(secretName))
-            throw new Exception("The secret name is required.");
+            throw new ArgumentNullException("The secret name is required.");
 
         try
         {
-            var keyVaultSecret = await secretClient.GetSecretAsync(secretName);
+            Response<KeyVaultSecret> keyVaultSecret = await secretClient.GetSecretAsync(secretName);                       
+            
+            if(keyVaultSecret == null) 
+                throw new Exception("Response<KeyVaultSecret> can not be null");
+            if (keyVaultSecret.Value == null)
+                throw new Exception("KeyVaultSecret can not be null");            
+                
             return keyVaultSecret.Value.Value;
         }
         catch(Exception ex) 
--- test/Looplex.DotNet.Services.Secrets.UnitTests/AzureSecretsServiceTests.cs
+++ test/Looplex.DotNet.Services.Secrets.UnitTests/AzureSecretsServiceTests.cs
@@ -1,43 +1,91 @@
-﻿using Looplex.DotNet.Core.Application.Abstractions.Services;
+﻿using Azure.Security.KeyVault.Secrets;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 
 namespace Looplex.DotNet.Services.Secrets.UnitTests
 {
     [TestClass]
-    public class AzureSecretsServiceTests
+    public class AzureSecretsServiceTests ()
     {
-        private ISecretsService _secretsService = null!;        
-
+        private AzureSecretsService _azureSecretService = null!;        
+        private SecretClient _secretClient = null!;  
+        
         [TestInitialize]
         public void Setup()
-        {
-            _secretsService = Substitute.For<ISecretsService>();
+        {            
+            _secretClient = Substitute.For<SecretClient>();            
+            _azureSecretService = new AzureSecretsService(_secretClient);
         }
 
-        [DataRow("looplex.com.br")]        
+        [DataRow("looplex.com.br")]
         [TestMethod]
         public async Task GetSecretAsync_ValueIsReturned_WhenSecretNameIsFound(string secretName)
         {
-            string? secretValue = await _secretsService.GetSecretAsync(secretName);
-            Assert.AreEqual("Hardcode", secretValue);
-        }
+            //Arrange
+            string returnedValue = "returnedValue";
+            var response = Substitute.For<Azure.Response<KeyVaultSecret>>();
+            var keyVaultSecret = new KeyVaultSecret(secretName, returnedValue);
 
-        [DataRow("nomeinexistente.com.br")]
-        [TestMethod]
-        public async void GetSecretAsync_NullIsReturned_WhenSecretNameIsNotFound(string secretName)
-        {
-            string? secretKey = await _secretsService.GetSecretAsync(secretName);
-            
-            Assert.AreEqual(null, secretKey);
-        }
+            response.Value.Returns(keyVaultSecret);
+            _secretClient.GetSecretAsync(secretName).Returns(Task.FromResult(response));
 
+            //Act
+            var result = await _azureSecretService.GetSecretAsync(secretName);
+
+            //Assert
+            result.Should().Be(returnedValue);
+        }
+                
         [DataRow(null)]
         [DataRow("")]
         [TestMethod]
         public void GetSecretAsync_ThrowException_WhenSecretNameIsNullOrEmpty(string secretName)
         {
-            Assert.ThrowsException<Exception>(async () => { await _secretsService.GetSecretAsync(secretName); });                     
+            //Arrange
+            _secretClient.GetSecretAsync(secretName);
+
+            //Act
+            var act = () => _azureSecretService.GetSecretAsync(secretName);
+
+            //Assert
+            var ex = Assert.ThrowsExceptionAsync<ArgumentNullException>(act);
+            Assert.AreEqual("The secret name is required", ex.Result.Message);
+        }
+
+        [DataRow("nomeinexistente.com.br")]
+        [TestMethod]
+        public void GetSecretAsync_ExceptionReturned_WhenValueIsNull(string secretName)
+        {
+            //Arrange                        
+            var response = Substitute.For<Azure.Response<KeyVaultSecret>>();
+            response.Value.ReturnsNull();
+            _secretClient.GetSecretAsync(secretName).Returns(Task.FromResult(response));
+
+            //Act
+            var act = () => _azureSecretService.GetSecretAsync(secretName);
+
+            //Assert
+            var ex = Assert.ThrowsExceptionAsync<Exception>(act);
+            Assert.AreEqual("KeyVaultSecret can not be null can not be null", ex.Result.Message);
+        }
+
+        [DataRow("nomeinexistente.com.br")]
+        [TestMethod]
+        public void GetSecretAsync_ExceptionReturned_WhenResponseIsNull(string secretName)
+        {
+            //Arrange
+            var response = Substitute.For<Azure.Response<KeyVaultSecret>>();
+            response = null;
+            _secretClient.GetSecretAsync(secretName).Returns(Task.FromResult(response));
+
+            //Act
+            var act = () => _azureSecretService.GetSecretAsync(secretName);
+
+            //Assert
+            var ex = Assert.ThrowsExceptionAsync<Exception>(act);
+            Assert.AreEqual("Response<KeyVaultSecret> can not be null", ex.Result.Message);
         }
     }
 }
