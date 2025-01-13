2bb852bc893e05f98a754a4e80815819ab9b809e
Fernando Cruz <fernando.cruz@looplex.com.br>
2025-01-13 19:28:01 -0300

    fix: Rabbit sugestions
    Conversation 2
    
    Create a configuration class
    Add configuration validation
    Use IOptions for dependency injection

---
+++ src/Looplex.DotNet.Services.Secrets/AzureSecretsOptions.cs
@@ -0,0 +1,18 @@
+﻿using Looplex.DotNet.Services.Secrets.Exceptions;
+
+namespace Looplex.DotNet.Services.Secrets
+{
+    public class AzureSecretsOptions
+    {
+        public string KeyVaultUrl { get; set; } = string.Empty;
+        
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(KeyVaultUrl))
+                throw new OptionsValidationException(
+                    nameof(AzureSecretsOptions),
+                    typeof(AzureSecretsOptions),
+                    new[] { $"The {nameof(KeyVaultUrl)} is required." });
+        }
+    }
+}
--- src/Looplex.DotNet.Services.Secrets/ExtensionMethods/SecretsExtensionMethods.cs
+++ src/Looplex.DotNet.Services.Secrets/ExtensionMethods/SecretsExtensionMethods.cs
@@ -3,6 +3,7 @@ using Azure.Security.KeyVault.Secrets;
 using Looplex.DotNet.Core.Application.Abstractions.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Looplex.DotNet.Services.Secrets.ExtensionMethods;
 
@@ -10,15 +11,17 @@ public static class SecretsExtensionMethods
 {   
     public static void AddSecretsServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddOptions<AzureSecretsOptions>()            
+            .Bind(configuration.GetSection("Azure:KeyVault"))
+            .ValidateOnStart();
+
         services.AddSingleton<ISecretsService, AzureSecretsService>();
         services.AddSingleton(_ =>
         {
-            string? azureKeyVaultUrl = configuration[Constants.AzureKeyVaultUrlKey];
-
-            if (string.IsNullOrEmpty(azureKeyVaultUrl))
-                throw new Exception($"The key {Constants.AzureKeyVaultUrlKey} was not found.");
-                        
-            return new SecretClient(new Uri(azureKeyVaultUrl), new DefaultAzureCredential());
+            var options = _.GetRequiredService<IOptions<AzureSecretsOptions>>().Value;
+            options.Validate();
+                                    
+            return new SecretClient(new Uri(options.KeyVaultUrl), new DefaultAzureCredential());
         });
     }
 
