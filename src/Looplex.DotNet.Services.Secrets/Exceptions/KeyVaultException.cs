6e0112b75a4c3381daf1fe52c95fff32e4c69a2e
Fernando Cruz <fernando.cruz@looplex.com.br>
2025-01-14 10:58:33 -0300

    fix: Rabbit Sugestions
    The exception message is not passed to the base constructor
    Constructor has an empty body
    Missing standard exception constructors for serialization and inner exceptions

--- src/Looplex.DotNet.Services.Secrets/Exceptions/KeyVaultException.cs
+++ src/Looplex.DotNet.Services.Secrets/Exceptions/KeyVaultException.cs
@@ -2,9 +2,18 @@
 {
     internal class KeyVaultException : Exception
     {
-        public KeyVaultException(string exception) : base()
+        public KeyVaultException() : base() 
+        {
+            
+        }
+        public KeyVaultException(string message) : base(message)
         {
 
-        }        
+        }
+
+        public KeyVaultException(string message, Exception innerException) : base(message, innerException)
+        {
+            
+        }
     }
 }
