819bc0917fb0e505f54df292d8892b17f2ec801e
Guilherme Câmara <guilherme.camara@outlook.com.br>
2025-01-15 10:40:24 -0300

    fix(di): added missing rbacservice registration

--- src/Looplex.DotNet.Services.Rbac/ExtensionMethod/RbacExtensionMethods.cs
+++ src/Looplex.DotNet.Services.Rbac/ExtensionMethod/RbacExtensionMethods.cs
@@ -1,4 +1,5 @@
 using Casbin;
+using Looplex.DotNet.Core.Application.Abstractions.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Looplex.DotNet.Services.Rbac.ExtensionMethod;
@@ -7,6 +8,7 @@ public static class RbacExtensionMethods
 {
     public static void AddRbacServices(this IServiceCollection services, IEnforcer enforcer)
     {
+        services.AddSingleton<IRbacService, RbacService>();
         services.AddSingleton(enforcer);
     }
 }
